e776951e803d6e7fdd980b083170e7dc6b7c9a3c
v4ygkj9zty-wq <v4ygkj9zty@privaterelay.appleid.com>
2026-09-22 12:49:10 +0200

    Refresh PDF OCR update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="vereinskassa-v6";
+const CACHE="vereinskassa-v7";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})()));
