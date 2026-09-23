e4b7c46078b6d1831ab7c50230827c9f2e17e63f
v4ygkj9zty-wq <v4ygkj9zty@privaterelay.appleid.com>
2026-09-23 20:52:12 +0200

    Publish receipt export v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="vereinskassa-v12";
+const CACHE="vereinskassa-v13";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})()));
